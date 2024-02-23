a8e219b5bed647abcfdb35d1cea526077dc26b8d
manogalo <johnny.faria@protonmail.com>
2024-02-23 12:17:05 -0300

    feat:starting page development

---
+++ vuetify-table-example/src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container>
+      <v-data-table
+        :items="chars"
+        item-key="name"
+        :loading="loading"
+        loading-text="Carregando tabela"
+      ></v-data-table>
+  </v-container>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    data() {
+      return {
+        chars: [],
+        loading: true
+      }
+    },
+    mounted() {
+      axios.get('https://swapi.dev/api/people/')
+        .then(response => {
+          this.chars = response.data.results
+          this.loading = false
+        }) 
+    }
+  }
+</script>
